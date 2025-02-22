a170dc3e260adc784fccfbc8b6eff780e6e4f711
CodeMaker46 <shikshak013@gmail.com>
2025-02-23 01:54:45 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -76,7 +76,9 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || '0.0.0.0';
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server running on port ${PORT} and host ${HOST}`);
 });
